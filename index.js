3525d10ec746411276fb61b8c84a519348fb4f17
spearsjonesco-coder <spearsjonesco@gmail.com>
2026-03-31 17:28:06 -0400

    Module-5

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
+// template_jmjmjt8
+// service_7usrhpq
 // 02HiRZLm8_5pZurip
-// <script type="text/javascript"
 
-        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
-</script>
-<script type="text/javascript">
-   (function(){
-      emailjs.init({
-        publicKey: "02HiRZLm8_5pZurip",
-      });
-   })();
-</script>
+function contact() {
+   console.log('it worked')
+}
