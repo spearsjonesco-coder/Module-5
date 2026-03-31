978b029837ed5c86e8ceb79a1b251bee9c7f8bc0
spearsjonesco-coder <spearsjonesco@gmail.com>
2026-03-31 18:58:04 -0400

    Module-5

--- index.js
+++ index.js
@@ -8,24 +8,26 @@ console.log(loading, scussess);
 
 function contact(event) {
    event.preventDefault();
-   console.log("it worked")
+   console.log("it worked");
 
-loading.classlist += " modal__overlay--visible"
-  setTimeout(() => {
-   console.log('it worked 1')
-  }, 500);
+loading.classlist += " modal__overlay--visible";
 
-  // emailjs
- //  .sendForm(
- //     'service_7usrhpq',
- //     'template_jmjmjt8',
- //     event.target,
- //     '02HiRZLm8_5pZurip',
- //  ).then(() => {
-  //    console.log('this worked1')
-  // })
- 
-
-
+emailjs
+.sendForm(
+      'service_7usrhpq',
+      'template_jmjmjt8',
+       event.target,
+       '02HiRZLm8_5pZurip'
+)
+.then(() => {
+  loading.classList.remove("modal__overlay--visible");
+  success.classList.add("modal__overlay--visible");
+})
+.catch(() => {
+   loading.classList.remove("modal__overlay--visible");
+   alert(
+      "The email service is temporarily unavailable. Please contace me directly at spearsjonesco@gmail.com.");
+   });
 
 }
+ 
