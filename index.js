3caa3f53de8619fe4d22803becc4e8bcb843af0e
spearsjonesco-coder <spearsjonesco@gmail.com>
2026-03-31 18:15:17 -0400

    Module-5

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-// template_jmjmjt8
-// service_7usrhpq
-// 02HiRZLm8_5pZurip
+// 
+// 
+// 
+const loading = document.querySelector('.modal__overlay--loading');
+const success = document.querySelector('.modal__overlay--success');
 
-function contact() {
-   console.log('it worked')
+console.log(loading, scussess);
+
+function contact(event) {
+   event.preventDefault();
+   console.log("it worked")
+
+loading.classlist += " modal__overlay--visible"
+  setTimeout(() => {
+   console.log('it worked 1')
+  }, 500);
+
+  // emailjs
+ //  .sendForm(
+ //     'service_7usrhpq',
+ //     'template_jmjmjt8',
+ //     event.target,
+ //     '02HiRZLm8_5pZurip',
+ //  ).then(() => {
+  //    console.log('this worked1')
+  // })
+ 
+
+
+
 }
