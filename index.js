04dcfdfc36379cf2bcb4952790922ecb3fe94773
spearsjonesco-coder <spearsjonesco@gmail.com>
2026-04-02 15:43:05 -0400

    module-5

--- index.js
+++ index.js
@@ -2,33 +2,34 @@
 // service_7usrhpq
 // 02HiRZLm8_5pZurip
 
-const loading = document.querySelector(".modal__overlay--loading");
-const success = document.querySelector(".modal__overlay--success");
-
-console.log(loading, scussess);
-
 function contact(event) {
    event.preventDefault();
-   console.log("it worked");
-
+const loading = document.querySelector('.modal__overlay--loading');
+const success = document.querySelector('.modal__overlay--success');
 loading.classlist += " modal__overlay--visible";
-
-emailjs
-.sendForm(
-      'service_7usrhpq',
-      'template_jmjmjt8',
-       event.target,
-       '02HiRZLm8_5pZurip'
-)
-.then(() => {
-  loading.classList.remove("modal__overlay--visible");
-  success.classList.add("modal__overlay--visible");
-})
-.catch(() => {
+ emailjs
+ .sendForm(
+       'service_7usrhpq',
+       'template_jmjmjt8',
+        event.target,
+        '02HiRZLm8_5pZurip'
+ ).then (() => {
+   loading.classList.remove("modal__overlay--visible");
+   success.classList += " modal__overlay--visible";
+  }).catch(() => {
    loading.classList.remove("modal__overlay--visible");
    alert(
-      "The email service is temporarily unavailable. Please contace me directly at spearsjonesco@gmail.com.");
-   });
-
+      "The email service is temporily unavailable. Please contact me directly at spearsjonesco@gmail.com."
+   );   
+  })
 }
- 
+
+let isModalOpen = false;
+function toggleModal() {
+   isModalOpen = !isModalOpen;
+   if (isModalOpen) {
+      return document.body.classList.remove("modal--open");
+   }
+   // toggle modal
+   document.body.classList += " modal--open";
+}
