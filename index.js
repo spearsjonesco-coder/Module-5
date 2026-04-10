20927d1e7d4bbccce5b8340acd294ca841fcd877
spearsjonesco-coder <spearsjonesco@gmail.com>
2026-04-10 15:24:42 -0400

    Module-5

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function moveBackground(event) {
    for (let i =0; i < shapes.length; ++i) {
       const isOdd = i % 2 !== 0;
       const boolInt = isOdd ? -1 : 1;
-      shapes[i].style.transform = 'translate(${x * boolInt}px, ${y * boolInt}px)'
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
    }
 }
 
